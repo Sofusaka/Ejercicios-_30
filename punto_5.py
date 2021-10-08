8568e37ec53312d86e8fb2a0107699616e61a0e4
Sofusaka <sofiasalas2010@gmial.com>
2021-10-08 10:55:58 -0500

    listicosssss

--- punto_5.py
+++ punto_5.py
@@ -1,2 +1,6 @@
 #Escribe un algoritmo o el respectivo diagrama de flujo que lea un número decimal e imprima su parte entera y su parte decimal por aparte.
 
+num=float(input(('Ingrese un numero decimal para hallar su parte entera y su parte decimal: ')))
+
+print(f'La parte entera del numero es {int(num)}')
+print(f'la parte decimal del numero es {num-int(num)}')
