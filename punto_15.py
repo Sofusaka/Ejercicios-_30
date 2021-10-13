b0dfee3813a3f1a3ed6d58b9c4a6d5e1e27923cc
Sofusaka <sofiasalas2010@gmial.com>
2021-10-13 00:12:00 -0500

    subidito tarde

---
+++ punto_15.py
@@ -0,0 +1,10 @@
+#Escribe un algoritmo o el respectivo diagrama de flujo que dadas coordenadas x1,y1 y x2,y2 en el plano cartesiano calcule la 
+# distancia entre ellos (considere todos los valores positivos)
+import math
+
+print('-----Calcular distancia entre dos puntos---------')
+x1=int(input('Ingrese la coordenada X del primer punto en el plano cartesiano: '))
+x2=int(input('Ingrese la coordenada y del segundo punto en el plano cartesiano: '))
+x3=int(input('Ingrese la coordenada X del segundo punto en el plano cartesiano: '))
+x4=int(input('Ingrese la coordenada y del segundo punto en el plano cartesiano: '))
+
--- punto__11.py
+++ punto__11.py
@@ -1,4 +1,5 @@
 #Escribe un algoritmo o el respectivo diagrama de flujo que determine el tiempo de caída de un objeto que se suelta desde una altura h.
+
 import math
 print('-----Calcular el tiempo de caida de un objeto desde altura h---------')
 h=float(input('Ingrese la altura desde la que caerá el objeto. Tenga en cuenta dar la distancia en metros (m): '))
