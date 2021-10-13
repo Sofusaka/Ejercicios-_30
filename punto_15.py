3bc54e8f2870de9e4929c07f28ffdc76a8c8ce9d
Sofusaka <sofiasalas2010@gmial.com>
2021-10-13 15:18:29 -0500

    FG

--- punto_15.py
+++ punto_15.py
@@ -4,7 +4,10 @@ import math
 
 print('-----Calcular distancia entre dos puntos---------')
 x1=int(input('Ingrese la coordenada X del primer punto en el plano cartesiano: '))
-x2=int(input('Ingrese la coordenada y del segundo punto en el plano cartesiano: '))
-x3=int(input('Ingrese la coordenada X del segundo punto en el plano cartesiano: '))
-x4=int(input('Ingrese la coordenada y del segundo punto en el plano cartesiano: '))
+y1=int(input('Ingrese la coordenada y del segundo punto en el plano cartesiano: '))
+x2=int(input('Ingrese la coordenada X del segundo punto en el plano cartesiano: '))
+y2=int(input('Ingrese la coordenada y del segundo punto en el plano cartesiano: '))
 
+dis=math.sqrt(((x2-x1)**2)+(y2-y1)**2)
+
+print(f'La distancia entre el punto ({x1},{y1}) y el punto ({x2},{y2}) es {dis}')
