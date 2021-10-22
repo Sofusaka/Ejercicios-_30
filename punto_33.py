23b6b05d757afb69fc75e7bb27a5285d1914e529
Sofusaka <sofiasalas2010@gmial.com>
2021-10-21 21:22:50 -0500

    akjhdk

---
+++ punto_33.py
@@ -0,0 +1,5 @@
+#Escribe un algoritmo o el respectivo diagrama de flujo que permita resolver una ecuación cuadrática
+#de tipo ax2 + bx + c (tenga en cuenta las todas las raíces, tanto las reales como las complejas).
+
+import math
+
